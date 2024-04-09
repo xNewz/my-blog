819dfcf82cbbcf7f482160d981e24153066d2a50
xNewz <pargornruasijan@gmail.com>
2024-04-09 21:36:20 +0700

    update: move main file to client

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import { Provider } from "./utils/Provider";
+import Navbar from "../components/Navbar";
+import { Provider } from "../utils/Provider";
 
 const firaCode = Fira_Code({ subsets: ["latin"] });
 
