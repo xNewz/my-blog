b5907123d8764bede9e6e5ba34e5efc5b9eb6b70
xNewz <pargornruasijan@gmail.com>
2024-04-09 21:31:09 +0700

    add: sanity project

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center h-16 w-full">
         <Link href="/">
           <div className={`${font.className} text-3xl dark:text-amber-50`}>
-            xNewz<span className="text-purple-500">Dev</span>
+            xNewz<span className="text-purple-500"> Dev</span>
           </div>
         </Link>
         <ThemeSwitch />
