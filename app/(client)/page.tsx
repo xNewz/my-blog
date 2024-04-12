cd2d9a95e4a7d98f1ff9c1119778a0b8b72a3683
xNewz <pargornruasijan@gmail.com>
2024-04-12 21:44:59 +0700

    add: get posts from CMS

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,7 +1,38 @@
-export default function Home() {
+import { client } from "@/sanity/lib/client";
+import Header from "../components/Header";
+import { Post } from "../utils/interface";
+import PostComponent from "../components/PostComponent";
+
+async function getPosts() {
+  const query = `
+  *[_type == "post"] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    tags[]-> {
+      _id,
+      slug,
+      name
+    }
+  }
+  `;
+  const data = await client.fetch(query);
+  return data;
+}
+
+export const revalidate = 60;
+
+export default async function Home() {
+  const posts: Post[] = await getPosts();
+
   return (
     <div>
-      <h1>Hello, world</h1>
+      <Header title="Articles" />
+      <div>
+        {posts?.length > 0 &&
+          posts?.map((post) => <PostComponent key={post?._id} post={post} />)}
+      </div>
     </div>
   );
 }
---
+++ app/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  title: string;
+}
+
+const Header = ({ title = "" }: Props) => {
+  return (
+    <header className="py-14 px-4 mb-12 text-center border-b dark:border-purple-900">
+      <h2 className="uppercase text-2xl mx-auto max-w-2xl font-bold">{title}</h2>
+    </header>
+  );
+};
+
+export default Header;
