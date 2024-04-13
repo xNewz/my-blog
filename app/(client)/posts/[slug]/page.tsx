b3808adb22b3a5471dd09f4d3a40ab77bdac5653
xNewz <pargornruasijan@gmail.com>
2024-04-13 20:11:48 +0700

    add: link to tags

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -5,6 +5,8 @@ import { Post } from "@/app/utils/interface";
 import { VT323 } from "next/font/google";
 import Link from "next/link";
 import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+import { urlForImage } from "@/sanity/lib/image";
 
 const dateFont = VT323({ weight: "400", subsets: ["latin"] });
 
@@ -41,12 +43,12 @@ const page = async ({ params }: Params) => {
     <div>
       <Header title={post?.title} />
       <div className="text-center">
-        <span className={`${dateFont.className} text-purple-500`}>
+        <span className={`${dateFont?.className} text-purple-500`}>
           {new Date(post?.publishedAt).toDateString()}
         </span>
-        <div className="mt-3">
+        <div className="mt-5">
           {post?.tags?.map((tag) => (
-            <Link key={tag._id} href={`/tag/${tag.slug.current}`}>
+            <Link key={tag?._id} href={`/tag/${tag.slug.current}`}>
               <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">
                 #{tag.name}
               </span>
@@ -54,7 +56,10 @@ const page = async ({ params }: Params) => {
           ))}
         </div>
         <div className={richTextStyles}>
-          <PortableText value={post?.body} />
+          <PortableText
+            value={post?.body}
+            components={myPortableTextComponents}
+          />
         </div>
       </div>
     </div>
@@ -62,6 +67,14 @@ const page = async ({ params }: Params) => {
 };
 
 export default page;
+
+const myPortableTextComponents = {
+  types: {
+    image: ({ value }: any) => (
+      <Image src={urlForImage(value)} alt="Post" width={700} height={700} />
+    ),
+  },
+};
 
 const richTextStyles = `
 mt-14
