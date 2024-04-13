2670962fd0043c2c6a6a2212f81cac99093859ad
xNewz <pargornruasijan@gmail.com>
2024-04-13 20:20:25 +0700

    add: not found page

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -1,12 +1,15 @@
 import Header from "@/app/components/Header";
-import React from "react";
-import { client } from "@/sanity/lib/client";
+import Toc from "@/app/components/Toc";
+import { slugify } from "@/app/utils/helpers";
 import { Post } from "@/app/utils/interface";
-import { VT323 } from "next/font/google";
-import Link from "next/link";
-import { PortableText } from "@portabletext/react";
-import Image from "next/image";
+import { client } from "@/sanity/lib/client";
 import { urlForImage } from "@/sanity/lib/image";
+import { PortableText } from "@portabletext/react";
+import { VT323 } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+import React from "react";
 
 const dateFont = VT323({ weight: "400", subsets: ["latin"] });
 
@@ -18,27 +21,35 @@ interface Params {
 
 async function getPost(slug: string) {
   const query = `
-    *[_type == "post" && slug.current == "${slug}"][0] {
-      title,
-      slug,
-      publishedAt,
-      excerpt,
+  *[_type == "post" && slug.current == "${slug}"][0] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    _id,
+    "headings": body[style in ["h2", "h3", "h4", "h5", "h6"]],
+    body,
+    tags[]-> {
       _id,
-      body,
-      tags[]-> {
-        _id,
-        slug,
-        name
-      }
+      slug,
+      name
     }
-    `;
+  }
+  `;
 
   const post = await client.fetch(query);
   return post;
 }
 
+export const revalidate = 60;
+
 const page = async ({ params }: Params) => {
   const post: Post = await getPost(params?.slug);
+
+  if (!post) {
+    notFound();
+  }
+
   return (
     <div>
       <Header title={post?.title} />
@@ -55,6 +66,7 @@ const page = async ({ params }: Params) => {
             </Link>
           ))}
         </div>
+        <Toc headings={post?.headings} />
         <div className={richTextStyles}>
           <PortableText
             value={post?.body}
@@ -71,7 +83,54 @@ export default page;
 const myPortableTextComponents = {
   types: {
     image: ({ value }: any) => (
-      <Image src={urlForImage(value)} alt="Post" width={700} height={700} />
+      <Image
+        src={urlForImage(value)}
+        alt="Post"
+        width={700}
+        height={700}
+      />
+    ),
+  },
+  block: {
+    h2: ({ value }: any) => (
+      <h2
+        id={slugify(value.children[0].text)}
+        className="text-3xl font-bold mb-3"
+      >
+        {value.children[0].text}
+      </h2>
+    ),
+    h3: ({ value }: any) => (
+      <h3
+        id={slugify(value.children[0].text)}
+        className="text-2xl font-bold mb-3"
+      >
+        {value.children[0].text}
+      </h3>
+    ),
+    h4: ({ value }: any) => (
+      <h4
+        id={slugify(value.children[0].text)}
+        className="text-2xl font-bold mb-3"
+      >
+        {value.children[0].text}
+      </h4>
+    ),
+    h5: ({ value }: any) => (
+      <h5
+        id={slugify(value.children[0].text)}
+        className="text-2xl font-bold mb-3"
+      >
+        {value.children[0].text}
+      </h5>
+    ),
+    h6: ({ value }: any) => (
+      <h6
+        id={slugify(value.children[0].text)}
+        className="text-xl font-bold mb-3"
+      >
+        {value.children[0].text}
+      </h6>
     ),
   },
 };
